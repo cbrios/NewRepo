c7fa71c377e7e9a4e501efe7e3bed0b9c08e5bf0
Chavela Rios <mv8865pr@go.minnstate.edu>
2020-09-23 12:56:52 -0500

    Comments added

--- Sum200/Program.cs
+++ Sum200/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Author : Chavela Rios
+// Date   : 2020-09-23
+// Chapter 5 exercise 9 Sum200
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
